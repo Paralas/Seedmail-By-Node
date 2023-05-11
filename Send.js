38c848dfc59cb3d14670d7773d1d6396464869f0
Urayukaki <preem.nakraf@gmail.com>
2023-05-12 01:12:52 +0700

    first commit

---
+++ Send.js
@@ -0,0 +1,36 @@
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth : {
+        user : '--',
+        pass : '--',
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+let video = fs.readFileSync('../Kafka_Home/Kafka_Internet.mp4');
+
+let mailOptions = {
+    from: '--',
+    to: '--',
+    subject: 'Kafka Ultimate Home',
+    text: 'Indi Home Phaket phoenix',
+    attachments: [{
+        filename: 'Kafka_Internet.mp4',
+        content: video
+    }]
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Message sent: ' + info.response);
+    }
+});
